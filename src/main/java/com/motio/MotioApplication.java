cfd8edc8d3b8380ad6305b9039359157cb8d2013
Filip Mróz <filip.mroz12@gmail.com>
2024-06-15 17:24:51 +0200

    Initial project setup.

---
+++ src/main/java/com/motio/MotioApplication.java
@@ -0,0 +1,13 @@
+package com.motio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotioApplication.class, args);
+	}
+
+}
