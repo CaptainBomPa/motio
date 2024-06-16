fef9e27ee6993a672cbe8cd56eaab6b897518547
Filip Mróz <filip.mroz12@gmail.com>
2024-06-16 09:24:56 +0200

    Configured data caching

--- src/main/java/com/motio/MotioApplication.java
+++ src/main/java/com/motio/MotioApplication.java
@@ -2,8 +2,10 @@ package com.motio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MotioApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/motio/repository/UserRepository.java
+++ src/main/java/com/motio/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.motio.repository;
 
 import com.motio.model.User;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +11,17 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Cacheable(value = "users", key = "#username")
     Optional<User> findByUsername(String username);
 
+    @Cacheable(value = "users", key = "#email")
     Optional<User> findByEmail(String email);
+
+    @CachePut(value = "users", key = "#result.username")
+    @Override
+    <S extends User> S save(S entity);
+
+    @CacheEvict(value = "users", key = "#entity.username")
+    @Override
+    void delete(User entity);
 }
